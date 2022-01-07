df0f6b2ea9c8b92a6037efa8eb7493f3af3a534c
Vladislav Mun <welastor@gmail.com>
2022-01-06 23:56:13 -0500

    initial set up is ready

---
+++ src/screens/Main_Screen/MainScreen.tsx
@@ -0,0 +1,11 @@
+import "./Styles/main-screen-styles.css";
+
+function MainScreen() {
+	return (
+		<div className="App">
+			<h2 className="hello">blue</h2>
+		</div>
+	);
+}
+
+export default MainScreen;

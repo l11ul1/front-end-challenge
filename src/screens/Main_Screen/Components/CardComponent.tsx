9c29c81c3fb4d9e96c10f44848a02b4ffe2c2b34
Vladislav Mun <welastor@gmail.com>
2022-01-14 14:38:55 -0500

    added toast when liked

--- src/screens/Main_Screen/Components/CardComponent.tsx
+++ src/screens/Main_Screen/Components/CardComponent.tsx
@@ -3,6 +3,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMeteor, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import Card from "../Types/Card";
 import { ModalContext } from "../../../helpers/ModalContext";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const CardComponent: React.FC<Card> = ({
 	cardTitle,
@@ -14,37 +16,60 @@ const CardComponent: React.FC<Card> = ({
 	const context = useContext(ModalContext);
 
 	return (
-		<div className="card" style={{ backgroundImage: `url(${cardImage})` }}>
-			<div className="card-content">
-				<h2 className="card-title">{cardTitle}</h2>
-				<p className="card-body">{cardDescription}</p>
-				<p className="card-date">{cardDate}</p>
-				<button
-					className={isLiked ? "liked" : "btn-like"}
-					onClick={() => {
-						setIsLiked(!isLiked);
-					}}
-				>
-					<FontAwesomeIcon icon={faMeteor} />
-				</button>
-				<button
-					className="btn-info"
-					onClick={() => {
-						const card: Card = {
-							cardTitle: cardTitle,
-							cardDescription: cardDescription,
-							cardDate: cardDate,
-							cardImage: cardImage,
-						};
-						context.setIsShown(!context.isShown);
-						context.setCard(card);
-					}}
-				>
-					<FontAwesomeIcon icon={faInfoCircle} />
-					More
-				</button>
+		<>
+			<div
+				className="card"
+				style={{ backgroundImage: `url(${cardImage})` }}
+			>
+				<div className="card-content">
+					<h2 className="card-title">{cardTitle}</h2>
+					<p className="card-body">{cardDescription}</p>
+					<p className="card-date">{cardDate}</p>
+					<button
+						className={isLiked ? "liked" : "btn-like"}
+						onClick={() => {
+							setIsLiked(!isLiked);
+							if (!isLiked) {
+								toast("🚀 Liked!", {
+									theme: "dark",
+									position: "bottom-center",
+									autoClose: 1500,
+									hideProgressBar: true,
+									closeOnClick: true,
+								});
+							} else {
+								toast("💔 Disliked!", {
+									theme: "dark",
+									position: "bottom-center",
+									autoClose: 1500,
+									hideProgressBar: true,
+									closeOnClick: true,
+								});
+							}
+						}}
+					>
+						<FontAwesomeIcon icon={faMeteor} />
+					</button>
+					<button
+						className="btn-info"
+						onClick={() => {
+							const card: Card = {
+								cardTitle: cardTitle,
+								cardDescription: cardDescription,
+								cardDate: cardDate,
+								cardImage: cardImage,
+							};
+							context.setIsShown(!context.isShown);
+							context.setCard(card);
+						}}
+					>
+						<FontAwesomeIcon icon={faInfoCircle} />
+						More
+					</button>
+				</div>
 			</div>
-		</div>
+			<ToastContainer />
+		</>
 	);
 };
 
